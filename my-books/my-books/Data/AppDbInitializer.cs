a8f4c748e3d5a73a6077135fadddb915dd038666
Luis Fernando M. Petreca <lfpetreca02@gmail.com>
2022-06-21 15:30:44 -0300

    setting entityframe work

---
+++ my-books/my-books/Data/AppDbInitializer.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using my_books.Data.Models;
+using System;
+using System.Linq;
+
+namespace my_books.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder apllicationBuilder)
+        {
+            using (var serviceScope = apllicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(
+                        new Book()
+                        {
+                            Title = "1st Book Title",
+                            Description = "1st Book Description",
+                            IsRead = true,
+                            DateRead = DateTime.Now.AddDays(-11),
+                            Rate = 4,
+                            Genre = "Biography",
+                            Author = "1st Author",
+                            CoverUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAcGXw78cemGGdEZXc0xWD8b6dURiojmB3oA&usqp=CAU",
+                            DateAdded = DateTime.Now,
+
+                        },
+                        new Book()
+                        {
+                            Title = "2nd Book Title",
+                            Description = "2nd Book Description",
+                            IsRead = false,                            
+                            Genre = "Biography",
+                            Author = "1st Author",
+                            CoverUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAcGXw78cemGGdEZXc0xWD8b6dURiojmB3oA&usqp=CAU",
+                            DateAdded = DateTime.Now,
+
+                        },
+                        new Book()
+                        {
+                            Title = "3rd Book Title",
+                            Description = "3rd Book Description",
+                            IsRead = true,
+                            DateRead = DateTime.Now.AddDays(-11),
+                            Rate = 4,
+                            Genre = "Biography",
+                            Author = "2nd Author",
+                            CoverUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAcGXw78cemGGdEZXc0xWD8b6dURiojmB3oA&usqp=CAU",
+                            DateAdded = DateTime.Now,
+
+                        }
+                    );
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
